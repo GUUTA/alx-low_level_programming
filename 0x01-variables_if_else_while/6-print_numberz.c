936279ace8fabea8beb1e17aac299c6e0cfc2dfb
GUUTA <guutatesema@gmail.com>
2022-06-05 06:49:08 -0700

    6-print-numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,13 +9,12 @@
 
 int main(void)
 {
-int n = 48;
-while (n <= 57)
-{
-putchar(n);
-n += 1;
+	int n = 48;
+	while (n <= 57)
+	{
+		putchar(n);
+		n += 1;
+	}
+	printf("\n");
+	return (0);
 }
-printf("\n");
-return (0);
-}
-
