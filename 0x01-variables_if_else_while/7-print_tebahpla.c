a7f97bbde038033d1db77149b9335681f9ff0b0c
GUUTA <guutatesema@gmail.com>
2022-06-05 11:57:48 -0700

    7-print

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	int n = 122;
-while (n > 96)
+int n = 122;
+     while (n > 96)
 	{
-		putchar(n);
-		n -= 1;
+	putchar(n);
+	n -= 1;
 	}
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }
