67ecda2c1cd31981a5ef79d3f408fdfe969fbc0c
GUUTA <guutatesema@gmail.com>
2022-06-08 00:29:13 -0700

    function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,15 +5,13 @@
  *   * You can only use _putchar twice in your code
  *    * Return: nothing.
  */
-
 void print_alphabet(void)
-{
+ {
 int l = 'a';
 while (l <= 'z')
-{
- _putchar(l);	
- l += 1;
-}
+  {
+_putchar(l);	
+l += 1;
+  }
 _putchar(10);
-}
-
+ }
