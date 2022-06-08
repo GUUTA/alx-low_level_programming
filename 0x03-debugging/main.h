aac8e836ffd80cab1512cb83c00892b995724b37
GUUTA <guutatesema@gmail.com>
2022-06-08 11:11:58 -0700

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,5 @@
-#ifndef GUTA_H
-#define GUTA_H
-
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
@@ -9,5 +8,4 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
-
-#endif // GUTA_H
+#endif /* MAIN_H */
