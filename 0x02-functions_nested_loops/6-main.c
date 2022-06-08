0d9f0dcafca75919dd3ec9ab34c3b7cdf3d97f4b
GUUTA <guutatesema@gmail.com>
2022-06-08 02:20:51 -0700

    Function

--- 0x02-functions_nested_loops/6-main.c
+++ 0x02-functions_nested_loops/6-main.c
@@ -1,16 +1,21 @@
-#include <stdio.h>
-#include <limits.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
-*  * main - check the code for Holberton School students.
+*  * main - check the code
 *   *
 *    * Return: Always 0.
 *     */
 int main(void)
 {
-int r;
-r = _abs(-1);
-printf("%d\n", r);
-return (0);
+	 int r;
+	  r = _abs(-1);
+	   printf("%d\n", r);
+	    r = _abs(0);
+	     printf("%d\n", r);
+	      r = _abs(1);
+	       printf("%d\n", r);
+	        r = _abs(-98);
+		 printf("%d\n", r);
+		  return (0);
 }
