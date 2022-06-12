34ac164b714424469d85da8d61ed4bbfc50ab752
GUUTA <guutatesema@gmail.com>
2022-06-12 09:26:41 -0700

    ifwhile

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-*  * main - program that prints all possible different combinations of two digits
-*   * Numbers must be separated by ,followed by a space
-*    * The two digits must be different
-*     * 01 and 10 are considered the same combination of the two digits 0 and 1
-*      * Return: 0
+* main - program that prints all possible different combinations of two digits
+* Numbers must be separated by ,followed by a space
+* The two digits must be different
+* 01 and 10 are considered the same combination of the two digits 0 and 1
+* Return: 0
 */
 
 int main(void)
