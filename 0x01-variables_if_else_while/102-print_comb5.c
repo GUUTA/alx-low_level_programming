2dbce319db81d7336362282bcf8e3562d3937fc9
GUUTA <guutatesema@gmail.com>
2022-06-12 10:03:27 -0700

    ifwhile

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
-* main - program that prints all possible combinations of
-* two two-digit
-* The numbers should range from 0 to 99
-* numbers must be separated by ,followed by a space
-* All numbers should be printed with two digits. 1 should be printed as 01
-* Return: 
+*main - program that prints all possible combinations of
+*two two-digit
+*The numbers should range from 0 to 99
+*numbers must be separated by ,followed by a space
+*All numbers should be printed with two digits. 1 should be printed as 01
+*Return: 
 */
+
 int main(void)
 {
 int n1 = 48;
