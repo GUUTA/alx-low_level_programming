ca2bd425541955329ce7dde72dd1f8a61d96e49a
GUUTA <guutatesema@gmail.com>
2022-06-13 03:07:25 -0700

    function

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
-*  * main - Prints the sum of even-valued Fibonacci sequence
-*   *        terms not exceeding 4000000.
-*     * Return: Always 0.
+* main - Prints the sum of even-valued Fibonacci sequence
+* terms not exceeding 4000000.
+* Return: Always 0.
 */
 int main(void)
 {
