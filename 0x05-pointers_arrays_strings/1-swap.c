9209528c22f0bb168568eedbbee24e57fa7e56d6
GUUTA <guutatesema@gmail.com>
2022-06-13 05:29:29 -0700

    array and pointer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* swap int-swaps the value of two int.
+* point to an int
+* point to other int 
+* return nothing
+*/
+void swap_int (int *a, int *b){
+int aux;
+aux = *a;
+*a = *b;
+*b = *a;
+}
