1d0d611041685ea467f0c1fee37fc7d208ab0ceb
GUUTA <guutatesema@gmail.com>
2022-06-17 00:00:49 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-*  * main - tests function that prints if integer is positive or negative
-*   * Return: 0
+*   * main - Test function for positive or negative
+*     * Return: 0
 */
 
 int main(void)
@@ -12,4 +12,3 @@ i = 0;
 positive_or_negative(i);
 return (0);
 }
-
