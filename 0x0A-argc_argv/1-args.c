74c780aef6dac737deefd7d7d363ac33fca1a1ae
GUUTA <guutatesema@gmail.com>
2022-06-24 23:58:26 -0700

    0x0A-argc-argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 /**
-*  * mn - prints its name, followed by a new line
-*   * @argc: n args
-*    * @argv: arr args
-*     * Return: 0
+*  *main - function
+*   *@argc: parameter 1
+*    *@argv: parameter2
+*     *Return: 0
 */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-if (argc > 0)
-printf("%d\n", argc - 1);
+int i;
+while (i < argc)
+{
+i++;
+}
+printf("%d\n", --i);
 return (0);
 }
