cce970b2df99cd226b372ba89ad7eaa3c54eb6d2
GUUTA <guutatesema@gmail.com>
2022-07-22 22:44:21 -0700

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,11 +12,11 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, y - 1));
+if (y < 0)
+return (-1);
+if (y == 0)
+return (1);
+return (x * _pow_recursion(x, y - 1));
 }
 
 /**
@@ -27,22 +27,22 @@ int _pow_recursion(int x, int y)
 
 void print_binary(unsigned long int n)
 {
-	unsigned int res_pow = 0;
-	int exp = 10;
-	int flag = 0;
-	if (n == 0)
-		_putchar('0');
-	while (exp >= 0)
-	{
-		res_pow = _pow_recursion(2, exp);
-		if (n >= res_pow)
-		{
-			_putchar('1');
-			n -= res_pow;
-			flag = 1;
-		}
-		else if (n < res_pow && flag == 1)
-			_putchar('0');
-		exp--;
+unsigned int res_pow = 0;
+int exp = 10;
+int flag = 0;
+if (n == 0)
+_putchar('0');
+while (exp >= 0)
+{
+res_pow = _pow_recursion(2, exp);
+if (n >= res_pow)
+{
+_putchar('1');
+n -= res_pow;
+flag = 1;
+}
+else if (n < res_pow && flag == 1)
+_putchar('0');
+exp--;
 }
 }
