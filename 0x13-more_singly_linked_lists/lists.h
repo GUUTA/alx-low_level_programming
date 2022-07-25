3cde31f98c0d2db5fc6c64b63d91eb54e589c889
GUUTA <guutatesema@gmail.com>
2022-07-24 23:17:00 -0700

    Input and output file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,10 @@
-#ifndef _LISTS_H
-#define _LISTS_H
+#ifndef _LIST_H_
+#define _LIST_H_
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -13,6 +18,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -28,4 +34,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-#endif /* #ifndef _LISTS_H */
+
+#endif
