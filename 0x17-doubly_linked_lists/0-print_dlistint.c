5a667f6b96a4155765f13860db67d28358dd0e6d
GUUTA <guutatesema@gmail.com>
2022-09-01 22:54:23 -0700

    doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a list
- *
- * @h: head pointer
- *
- * Return: the number of nodes
+ * print_dlistint - function that prints all the elements of a dlistint_t list
+ * @h: header pointer
+ * Return: number of nodes
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-size_t num_nodes = 0;
-while (h != NULL)
-{
-printf("%d\n", h->n);
-h = h->next;
-num_nodes++;
-}
-return (num_nodes);
+	size_t cont = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		cont++;
+	}
+	return (cont);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,9 +1,5 @@
-#ifndef _LISTS_
-#define _LISTS_
-
-#include <stdio.h>
-#include <stdlib.h>
-
+#ifndef DLISTS_H
+#define DLISTS_H
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -11,6 +7,7 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
+ * for Holberton project
  */
 typedef struct dlistint_s
 {
@@ -18,7 +15,6 @@ typedef struct dlistint_s
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
 } dlistint_t;
-
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
@@ -28,5 +24,4 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-
-#endif
+#endif /* #ifndef DLISTS_H */
