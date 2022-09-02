de634efee9e5c6f99054ad227be1cde15e163356
GUUTA <guutatesema@gmail.com>
2022-09-01 22:37:42 -0700

    doublylinkedlist

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,13 +9,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t num_nodes = 0;
-
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		num_nodes++;
-	}
-	return (num_nodes);
+size_t num_nodes = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+num_nodes++;
+}
+return (num_nodes);
 }
